9063f4670d95bce83350a5fb6353cde30b430aac
Orgho McGuire <mcguireon@gmail.com>
2020-10-01 19:37:31 +0600

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+
+    if word in data:
+        return data[word]
+
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+    
+    else:
+        return "The word doesn't exist. Please check it"
+
 
 word = input("Enter a Word: ")
 
